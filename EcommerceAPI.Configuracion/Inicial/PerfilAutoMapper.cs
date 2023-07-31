ea98936bbcdbc0de716acc677bfdf38203e838dc
silva <silva@LAPTOP-4Q3B3R1J>
2023-07-31 15:46:14 -0500

    Agregar archivos de proyecto.

---
+++ EcommerceAPI.Configuracion/Inicial/PerfilAutoMapper.cs
@@ -0,0 +1,15 @@
+﻿
+using AutoMapper;
+using EcommerceAPI.Comunes.Classes.Contracts.Ecommerce;
+using EcommerceAPI.Infraestructura.Database.Entities;
+
+namespace EcommerceAPI.Configuracion.Inicial
+{
+    public class PerfilAutoMapper : Profile
+    {
+        public PerfilAutoMapper() 
+        {
+            CreateMap<ClienteEntity, ClienteContract>().ReverseMap();
+        }
+    }
+}
---
+++ EcommerceAPI.Infraestructura/Database/Context/EcommerceContext.cs
@@ -0,0 +1,27 @@
+﻿using EcommerceAPI.Infraestructura.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Infraestructura.Database.Context
+{
+    public class EcommerceContext : DbContext
+    {
+        public EcommerceContext() { } 
+        public EcommerceContext(DbContextOptions<EcommerceContext> options):base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+
+        #region DBSets
+            public virtual DbSet<ClienteEntity> Clientes { get; set;}    
+        #endregion
+    }
+}
